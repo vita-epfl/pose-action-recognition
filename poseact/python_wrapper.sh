08844016ae4ead914d485723d69ed0a1390f75d4
Weijiang Xiong <weijiangxiong1998@gmail.com>
2021-12-13 19:54:48 +0100

    modify default settings

--- poseact/python_wrapper.sh
+++ poseact/python_wrapper.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name RUN_TITAN
 #SBATCH --account=vita
-#SBATCH --reservation=VITA
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node=1
+#SBATCH --ncpus-per-task=10
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --qos=gpu
-#SBATCH --time 18:00:00
+#SBATCH --time 72:00:00
 #SBATCH --output "out/slurm-%A_%a.log"
-#SBATCH --mem=0
+#SBATCH --mem=32G
 
+echo started at `date`
 module load gcc/8.4.0-cuda cuda/10.2.89
 echo "${@:1}"
 python -u "${@:1}"
+wait 
+echo finished at `date`
